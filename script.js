240b6dcd4ea4a797d5077e53d56f6807d5de3549
Nitin kumar <149141072+nitinkumar09@users.noreply.github.com>
2023-11-13 02:08:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let signupbtn = document.getElementById("signupbtn");
+let signinbtn = document.getElementById("signinbtn");
+let namefield = document.getElementById("namefield");
+let title = document.getElementById("title");
+
+signinbtn.onclick = function()
+{
+    namefield.style.maxHeight = "0";
+    title.innerHTML = "sign in";
+    signupbtn.classList.add("disable");
+    signinbtn.classList.remove("disable");
+
+}
+signupbtn.onclick = function()
+{
+    namefield.style.maxHeight = "60px";
+    title.innerHTML = "sign up";
+    signupbtn.classList.remove("disable");
+    signinbtn.classList.add("disable");
+
+    //location.reload();
+}
